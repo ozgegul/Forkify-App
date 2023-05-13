2adce98e0fe31d3c4f9ed4367ffe093c7687a2b1
ozgegul <ozgegul1@icloud.com>
2023-05-13 23:46:13 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ed26a736-91e6-4ab8-ba8d-0748968c8f69';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,15 @@
+import View from './View';
+import previewView from './previewView';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'We could not find that recipe! Please try again :)';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
